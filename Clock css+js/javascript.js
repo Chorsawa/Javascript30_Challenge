f097caef4954bd7a9ae2a3b74257b2d4ee5b03dd
Joanna <80103635+Chorsawa@users.noreply.github.com>
2021-03-22 16:24:23 +0100

    Add files via upload

---
+++ Clock css+js/javascript.js
@@ -0,0 +1,21 @@
+const secondsHand = document.querySelector('.handSeconds');
+const minutesHand = document.querySelector('.handMinutes');
+const hoursHand = document.querySelector('.handHours');
+
+function setDate(){
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds /60) * 360) + 90;
+    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes /60) * 360) + 90;
+    minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours /12) * 360) + 90;
+    hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+};
+    
+setInterval(setDate, 1000);
